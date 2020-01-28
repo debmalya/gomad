cf24a3c16fd551a1a72c4f3c14db8818a08235aa
Debmalya Jash (deb) <debmalya.jash.e@thalesdigital.io>
2020-01-28 09:39:01 +0800

    Removed unnecessary code

--- server.go
+++ server.go
@@ -14,113 +14,7 @@ import (
 	"github.com/debmalya/gomad/router"
 )
 
-/*
-type User struct {
-	Name  string `json:"name" xml:"name" form:"name" query:"name"`
-	Email string `json:"email" xml:"email" form:"email" query:"email"`
-}
-
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, do you hear echo!")
-	})
-	e.POST("/users", func(c echo.Context) error {
-		u := new(User)
-		if err := c.Bind(u); err != nil {
-			return err
-		}
-		return c.JSON(http.StatusCreated, u)
-		// or
-		// return c.XML(http.StatusCreated, u)
-	})
-	e.GET("/users/:id", getUser)
-	// e.PUT("/users/:id", updateUser)
-	// e.DELETE("/users/:id", deleteUser)
-	e.Static("/static", "static")
-
-	// Root level middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	//CORS
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-	}))
-
-	// Group level middleware
-	g := e.Group("/admin")
-	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		if username == "joe" && password == "secret" {
-			return true, nil
-		}
-		return false, nil
-	}))
-
-	// Route level middleware
-	track := func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			println("request to /users")
-			return next(c)
-		}
-	}
-	e.GET("/users", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users")
-	}, track)
-
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// e.GET("/users/:id", getUser)
-func getUser(c echo.Context) error {
-	// User ID from path `users/:id`
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-}
-
-//e.GET("/show", show)
-func show(c echo.Context) error {
-	// Get team and member from the query string
-	team := c.QueryParam("team")
-	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:"+team+", member:"+member)
-}
-
-// e.POST("/save", save)
-func save(c echo.Context) error {
-	// Get name
-	name := c.FormValue("name")
-	// Get avatar
-	avatar, err := c.FormFile("avatar")
-	if err != nil {
-		return err
-	}
-
-	// Source
-	src, err := avatar.Open()
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-
-	// Destination
-	dst, err := os.Create(avatar.Filename)
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
-
-	// Copy
-	if _, err = io.Copy(dst, src); err != nil {
-		return err
-	}
-
-	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
-}
-*/
-
-// Setup echo
+// SetupEcho to setup echo framework
 func SetupEcho() *echo.Echo {
 	e := echo.New()
 	defer e.Close()
@@ -143,9 +37,9 @@ func main() {
 	gracefulShutdown(s, 5*time.Second)
 }
 
-// Setup server
+// SetupServer - To setup the server
 func SetupServer(e *echo.Echo) *http.Server {
-	s := &http.Server{Addr: "localhost:1971", Handler: e}
+	s := &http.Server{Addr: ":1971", Handler: e}
 	done := make(chan bool)
 	go func() {
 		log.Info("Server exit:", s.ListenAndServe())
